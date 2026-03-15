d70a123968323f2f02a4eab02304e0a4fa9ce583
Akendo <ruslankadirov1337@gmail.com>
2026-03-15 22:04:14 +0500

    refactor(identity-service): update dtos

--- identity-service/src/main/java/akendo/identityservice/dto/LoginRequest.java
+++ identity-service/src/main/java/akendo/identityservice/dto/LoginRequest.java
@@ -1,12 +1,7 @@
 package akendo.identityservice.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class LoginRequest {
-
-    private String email;
-    private String password;
+public record LoginRequest(
+        String email,
+        String password)
+{
 }
